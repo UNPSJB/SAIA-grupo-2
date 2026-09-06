c71ffc9e5bac8e1087774199fc9c1168d1dcba43
AmirAle <Amir___1@outlook.com>
2026-09-06 13:32:30 -0300

    (wip): Implementacion de backend Empleado

---
+++ backend/src/empleados/constants.py
@@ -0,0 +1,3 @@
+class ErrorCode:
+    EMPLEADO_NO_ENCONTRADO = "El empleado no fue encontrado"
+    NOMBRE_DUPLICADO = "El nombre ya existe"
---
+++ backend/src/empleados/services.py
@@ -0,0 +1,45 @@
+import logging
+from typing import List
+from sqlalchemy import delete, select, update
+from sqlalchemy.orm import Session
+from src.empleados.models import Empleado
+from src.empleados import schemas, exceptions
+
+# Creacion de logger para modulo
+logger=logging.getLogger(__name__)
+
+# CRUD
+
+def crear_empleado(db: Session, empleado:schemas.EmpleadoCreate) -> schemas.Empleado:
+    _empleado = Empleado(**empleado.model_dump())
+    db.add(_empleado)
+    db.commit()
+    db.refresh(_empleado)
+    return _empleado
+
+def listar_empleados(db: Session) -> List[schemas.Empleado]:
+    logger.info("Listando empleados desde services")
+    return db.scalars(select(Empleado)).all()
+
+def leer_empleado(db: Session, empleado_id: int) -> schemas.Empleado:
+    db_empleado = db.scalar(select(Empleado).where(Empleado.id == empleado_id))
+    if db_empleado is None:
+        raise exceptions.EmpleadoNoEncontrado()
+    return db_empleado
+
+def modificar_empleado(
+    db: Session, empleado_id: int, empleado: schemas.EmpleadoUpdate
+) -> Empleado:
+    db_empleado = leer_empleado(db, empleado_id)
+    db.execute(
+        update(Empleado).where(Empleado.id == empleado_id).values(**empleado.model_dump())
+    )
+    db.commit()
+    db.refresh(db_empleado)
+    return db_empleado
+
+def eliminar_empleado(db: Session, empleado_id: int) -> schemas.Empleado:
+    db_empleado = leer_empleado(db, empleado_id)
+    db.execute(delete(Empleado).where(Empleado.id == empleado_id))
+    db.commit()
+    return db_empleado
